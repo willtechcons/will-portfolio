121110d2d72c4b614b7f9338b85dfe0a67fa6cfb
Will Nguyen <willng.personal@gmail.com>
2025-03-14 22:40:10 +0800

    big update

--- src/components/Service.js
+++ src/components/Service.js
@@ -38,29 +38,49 @@ const Service = () => {
       <div className="container">
         <div className="row align-items-center">
           <div className="service-title-container">
+            {/* Service Title */}
             <div className="section_title wow fadeInUp">
               <p>Skills</p>
               <h2>My Core Skills & Expertise</h2>
             </div>
+            {/* Arrow icon */}
             <div className="service-btn-container wow fadeInUp">
               <a href="#" className="slider-arrow service-swiper-button-left">
-                <img className="svg" src="/images/icons/arrow-left.svg" alt="service left btn" />
+                <img
+                  className="svg"
+                  src="images/icons/arrow-left.svg"
+                  alt="service left btn"
+                />
               </a>
-              <a href="#" className="slider-arrow active service-swiper-button-right">
-                <img className="svg" src="/images/icons/arrow-right.svg" alt="service right btn" />
+              <a
+                href="#"
+                className="slider-arrow active service-swiper-button-right"
+              >
+                <img
+                  className="svg"
+                  src="images/icons/arrow-right.svg"
+                  alt="service left btn"
+                />
               </a>
             </div>
           </div>
-
-          <Swiper {...doraSlider.serviceSlider} className="swiper services-cont wow fadeInUp">
+          <Swiper
+            {...doraSlider.serviceSlider}
+            className="swiper services-cont wow fadeInUp"
+          >
+            {" "}
             {serviceData.map((service, i) => (
               <SwiperSlide className="swiper-slide" key={i}>
                 <div className="service-item">
-                  <span className={`service-item-logo service-item-logo-cont-${service.id}`}>
+                  <span
+                    className={`service-item-logo service-item-logo-cont-${service.id}`}
+                  >
                     <img src={service.icon} alt="service" />
                   </span>
                   <h4>{service.title}</h4>
-                  <p>{service.des}</p>
+                  <p>
+                    {service.des}
+                  </p>
                 </div>
               </SwiperSlide>
             ))}
@@ -70,5 +90,4 @@ const Service = () => {
     </section>
   );
 };
-
 export default Service;
