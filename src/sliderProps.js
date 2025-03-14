850df922b45f975f03af0fb0ec65a17056824248
Will Nguyen <willng.personal@gmail.com>
2025-03-14 23:32:16 +0800

    slider

--- src/sliderProps.js
+++ src/sliderProps.js
@@ -1,61 +1,30 @@
-import SwiperCore, {
-  Autoplay,
-  EffectCreative,
-  EffectFade,
-  Grid,
-  Mousewheel,
-  Navigation,
-  Pagination,
-  Virtual,
-} from "swiper";
-SwiperCore.use([
-  Mousewheel,
-  Pagination,
-  Navigation,
-  EffectFade,
-  Autoplay,
-  Grid,
-  EffectCreative,
-  Virtual,
-]);
+import { Navigation, Pagination, Autoplay, EffectFade } from "swiper/modules";
+import { Swiper } from "swiper/react";
+
+// Kích hoạt các module của Swiper
+Swiper.use([Navigation, Pagination, Autoplay, EffectFade]);
 
 export const doraSlider = {
   serviceSlider: {
     slidesPerView: 4,
     spaceBetween: 30,
     loop: true,
-    centerSlider: true,
-    fade: true,
     grabCursor: true,
+    fade: true,
+    autoplay: {
+      delay: 3000, // Tự động trượt sau 3s
+      disableOnInteraction: false,
+    },
     navigation: {
       nextEl: ".service-swiper-button-right",
       prevEl: ".service-swiper-button-left",
     },
     breakpoints: {
-      // when window width is >= 320px
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 30,
-      },
-      // when window width is >= 320px
-      574: {
-        slidesPerView: 2,
-        spaceBetween: 20,
-      },
-      // when window width is >= 766px
-      860: {
-        slidesPerView: 2,
-        spaceBetween: 25,
-      },
-      1000: {
-        slidesPerView: 3,
-        spaceBetween: 25,
-      },
-      // when window width is >= 1024px
-      1240: {
-        slidesPerView: 4,
-        spaceBetween: 30,
-      },
+      320: { slidesPerView: 1, spaceBetween: 30 }, // Điện thoại nhỏ
+      574: { slidesPerView: 2, spaceBetween: 20 }, // Điện thoại lớn
+      860: { slidesPerView: 2, spaceBetween: 25 }, // Máy tính bảng
+      1000: { slidesPerView: 3, spaceBetween: 25 }, // Laptop
+      1240: { slidesPerView: 4, spaceBetween: 30 }, // Màn hình lớn
     },
   },
   feedbackSlider: {
@@ -63,16 +32,16 @@ export const doraSlider = {
     spaceBetween: 30,
     loop: true,
     grabCursor: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
     navigation: {
       nextEl: ".feedback-right",
       prevEl: ".feedback-left",
     },
     breakpoints: {
-      // when window width is >= 1024px
-      1400: {
-        slidesPerView: 2,
-        spaceBetween: 30,
-      },
+      1400: { slidesPerView: 2, spaceBetween: 30 }, // Chỉ thay đổi khi >= 1400px
     },
   },
 };
