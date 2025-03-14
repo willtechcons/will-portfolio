02ce041786b3f76abefa0c899ee3df399ff5cc28
Will Nguyen <willng.personal@gmail.com>
2025-03-14 23:01:12 +0800

    slider

--- src/sliderProps.js
+++ src/sliderProps.js
@@ -1,23 +1,9 @@
-import SwiperCore, {
-  Autoplay,
-  EffectCreative,
-  EffectFade,
-  Grid,
-  Mousewheel,
-  Navigation,
-  Pagination,
-  Virtual,
-} from "swiper";
-SwiperCore.use([
-  Mousewheel,
-  Pagination,
-  Navigation,
-  EffectFade,
-  Autoplay,
-  Grid,
-  EffectCreative,
-  Virtual,
-]);
+import { Swiper } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/effect-fade";
+import { Navigation, Pagination, Autoplay, EffectFade, Grid } from "swiper/modules";
 
 export const doraSlider = {
   serviceSlider: {
@@ -25,54 +11,27 @@ export const doraSlider = {
     spaceBetween: 30,
     loop: true,
     centerSlider: true,
-    fade: true,
     grabCursor: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
     navigation: {
       nextEl: ".service-swiper-button-right",
       prevEl: ".service-swiper-button-left",
     },
-    breakpoints: {
-      // when window width is >= 320px
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 30,
-      },
-      // when window width is >= 320px
-      574: {
-        slidesPerView: 2,
-        spaceBetween: 20,
-      },
-      // when window width is >= 766px
-      860: {
-        slidesPerView: 2,
-        spaceBetween: 25,
-      },
-      1000: {
-        slidesPerView: 3,
-        spaceBetween: 25,
-      },
-      // when window width is >= 1024px
-      1240: {
-        slidesPerView: 4,
-        spaceBetween: 30,
-      },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
     },
-  },
-  feedbackSlider: {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    grabCursor: true,
-    navigation: {
-      nextEl: ".feedback-right",
-      prevEl: ".feedback-left",
-    },
+    effect: "fade",
+    modules: [Navigation, Pagination, Autoplay, EffectFade, Grid],
     breakpoints: {
-      // when window width is >= 1024px
-      1400: {
-        slidesPerView: 2,
-        spaceBetween: 30,
-      },
+      320: { slidesPerView: 1, spaceBetween: 30 },
+      574: { slidesPerView: 2, spaceBetween: 20 },
+      860: { slidesPerView: 2, spaceBetween: 25 },
+      1000: { slidesPerView: 3, spaceBetween: 25 },
+      1240: { slidesPerView: 4, spaceBetween: 30 },
     },
   },
 };
