c7b7fa0c5ea0783efa048272aeb4a33512edc143
Will Nguyen <willng.personal@gmail.com>
2025-03-14 22:47:57 +0800

    sP

--- src/sliderProps.js
+++ src/sliderProps.js
@@ -8,8 +8,6 @@ import SwiperCore, {
   Pagination,
   Virtual,
 } from "swiper";
-
-// Kích hoạt các module Swiper cần thiết
 SwiperCore.use([
   Mousewheel,
   Pagination,
@@ -25,20 +23,39 @@ export const doraSlider = {
   serviceSlider: {
     slidesPerView: 4,
     spaceBetween: 30,
-    loop: true, // Cho phép slider lặp lại liên tục
-    speed: 600, // Tăng tốc độ slide (mặc định 300)
-    grabCursor: true, // Hiển thị trỏ chuột dạng bàn tay khi hover
-    mousewheel: true, // Cho phép lướt bằng chuột
+    loop: true,
+    centerSlider: true,
+    fade: true,
+    grabCursor: true,
     navigation: {
       nextEl: ".service-swiper-button-right",
       prevEl: ".service-swiper-button-left",
     },
     breakpoints: {
-      320: { slidesPerView: 1, spaceBetween: 30 },
-      574: { slidesPerView: 2, spaceBetween: 20 },
-      860: { slidesPerView: 2, spaceBetween: 25 },
-      1000: { slidesPerView: 3, spaceBetween: 25 },
-      1240: { slidesPerView: 4, spaceBetween: 30 },
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
+      // when window width is >= 320px
+      574: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      // when window width is >= 766px
+      860: {
+        slidesPerView: 2,
+        spaceBetween: 25,
+      },
+      1000: {
+        slidesPerView: 3,
+        spaceBetween: 25,
+      },
+      // when window width is >= 1024px
+      1240: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
     },
   },
   feedbackSlider: {
@@ -46,13 +63,16 @@ export const doraSlider = {
     spaceBetween: 30,
     loop: true,
     grabCursor: true,
-    mousewheel: true, // Thêm vuốt chuột vào feedback slider
     navigation: {
       nextEl: ".feedback-right",
       prevEl: ".feedback-left",
     },
     breakpoints: {
-      1400: { slidesPerView: 2, spaceBetween: 30 },
+      // when window width is >= 1024px
+      1400: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
     },
   },
 };
